f37d6593c596409b5d332368ba2b8caacc9d6895
Khalid <22-46601-1@student.aiub.edu>
2023-12-02 01:08:07 +0600

    Added custom roundedPanel Control

---
+++ CustomPanel/RoundedPanel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPanel
+{
+    public class RoundedPanel : Panel
+    {
+        private int _radius = 10;
+        public int Radius
+        {
+            get { return _radius; }
+            set
+            {
+                _radius = value;
+                Invalidate();
+            }
+        }
+
+        private SolidBrush _backgroundBrush = new SolidBrush(SystemColors.Control);
+        private Color _backgroundColor = SystemColors.Control;
+        public Color BackgroundColor
+        {
+            get { return _backgroundColor; }
+            set
+            {
+                _backgroundBrush = new SolidBrush(_backgroundColor = value);
+                Invalidate();
+            }
+
+        }
+
+        private Color _borderColor = SystemColors.Control;
+        private Pen _borderPen = new Pen(ControlPaint.Light(SystemColors.Control, 0.0f), 0);
+        public Color BorderColor
+        {
+            get { return _borderColor; }
+            set
+            {
+                _borderColor = value;
+                _borderPen = new Pen(ControlPaint.Light(_borderColor, 0.0f), _borderWidth);
+                Invalidate();
+            }
+        }
+
+        private float _borderWidth = 1.0f;
+        public float BorderWidth
+        {
+            get { return _borderWidth; }
+            set
+            {
+                _borderWidth = value;
+                _borderPen = new Pen(ControlPaint.Light(_borderColor, 0.0f), _borderWidth);
+                Invalidate();
+            }
+        }
+
+        private void drawBorder(Graphics g) =>
+            g.DrawRoundedRectangle(_borderPen, 10, 10, Width - 20, Height - 20, _radius);
+
+        private void drawBackground(Graphics g) =>
+                    g.FillRoundedRectangle(_backgroundBrush, 10, 10, Width - 20, Height - 20, _radius);
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            drawBorder(g);
+            drawBackground(g);
+        }
+    }
+}
--- MusicPlayer/SearchForm.Designer.cs
+++ MusicPlayer/SearchForm.Designer.cs
@@ -30,23 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchForm));
             splitContainer1 = new SplitContainer();
-            customPanel1 = new CustomPanel.CustomPanel();
-            customPanel2 = new CustomPanel.CustomPanel();
             tableLayoutPanel1 = new TableLayoutPanel();
             tableLayoutPanel2 = new TableLayoutPanel();
             searchBox = new TextBox();
             searchBtn = new Button();
             panel1 = new Panel();
             searchResultPanel = new FlowLayoutPanel();
+            roundedPanel1 = new CustomPanel.RoundedPanel();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
-            customPanel1.SuspendLayout();
-            customPanel2.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
             panel1.SuspendLayout();
+            roundedPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // splitContainer1
@@ -59,47 +57,15 @@
             // 
             // splitContainer1.Panel1
             // 
-            splitContainer1.Panel1.Controls.Add(customPanel1);
+            splitContainer1.Panel1.Controls.Add(roundedPanel1);
             // 
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(panel1);
             splitContainer1.Size = new Size(683, 588);
-            splitContainer1.SplitterDistance = 58;
+            splitContainer1.SplitterDistance = 80;
             splitContainer1.SplitterWidth = 1;
             splitContainer1.TabIndex = 0;
-            // 
-            // customPanel1
-            // 
-            customPanel1.BackColor = Color.FromArgb(55, 65, 81);
-            customPanel1.BorderRadius = 50;
-            customPanel1.Controls.Add(customPanel2);
-            customPanel1.Dock = DockStyle.Fill;
-            customPanel1.ForeColor = Color.Black;
-            customPanel1.GradientAngle = 0F;
-            customPanel1.GradientBottomColor = Color.FromArgb(55, 65, 81);
-            customPanel1.GradientTopColor = Color.FromArgb(55, 65, 81);
-            customPanel1.Location = new Point(0, 0);
-            customPanel1.Name = "customPanel1";
-            customPanel1.Padding = new Padding(2);
-            customPanel1.Size = new Size(683, 58);
-            customPanel1.TabIndex = 0;
-            // 
-            // customPanel2
-            // 
-            customPanel2.BackColor = Color.FromArgb(13, 18, 47);
-            customPanel2.BorderRadius = 50;
-            customPanel2.Controls.Add(tableLayoutPanel1);
-            customPanel2.Dock = DockStyle.Fill;
-            customPanel2.ForeColor = Color.Black;
-            customPanel2.GradientAngle = 90F;
-            customPanel2.GradientBottomColor = Color.FromArgb(13, 18, 47);
-            customPanel2.GradientTopColor = Color.FromArgb(13, 18, 47);
-            customPanel2.Location = new Point(2, 2);
-            customPanel2.Name = "customPanel2";
-            customPanel2.Padding = new Padding(10, 5, 10, 5);
-            customPanel2.Size = new Size(679, 54);
-            customPanel2.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
@@ -110,13 +76,13 @@
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
             tableLayoutPanel1.Controls.Add(searchBtn, 1, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(10, 5);
+            tableLayoutPanel1.Location = new Point(15, 15);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.Padding = new Padding(10, 0, 0, 0);
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(659, 44);
+            tableLayoutPanel1.Size = new Size(653, 50);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -131,7 +97,7 @@
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.Size = new Size(562, 38);
+            tableLayoutPanel2.Size = new Size(556, 44);
             tableLayoutPanel2.TabIndex = 2;
             // 
             // searchBox
@@ -141,10 +107,10 @@
             searchBox.Dock = DockStyle.Fill;
             searchBox.Font = new Font("Segoe UI", 15F);
             searchBox.ForeColor = Color.White;
-            searchBox.Location = new Point(3, 2);
+            searchBox.Location = new Point(3, 5);
             searchBox.Name = "searchBox";
             searchBox.PlaceholderText = "Search...";
-            searchBox.Size = new Size(556, 34);
+            searchBox.Size = new Size(550, 34);
             searchBox.TabIndex = 1;
             searchBox.KeyDown += SearchBox_KeyDown;
             // 
@@ -154,10 +120,10 @@
             searchBtn.FlatAppearance.BorderSize = 0;
             searchBtn.FlatStyle = FlatStyle.Flat;
             searchBtn.Image = (Image)resources.GetObject("searchBtn.Image");
-            searchBtn.Location = new Point(578, 0);
+            searchBtn.Location = new Point(572, 0);
             searchBtn.Margin = new Padding(0);
             searchBtn.Name = "searchBtn";
-            searchBtn.Size = new Size(81, 44);
+            searchBtn.Size = new Size(81, 50);
             searchBtn.TabIndex = 1;
             searchBtn.UseVisualStyleBackColor = true;
             searchBtn.Click += Search_Btn_Click;
@@ -173,7 +139,7 @@
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Padding = new Padding(0, 20, 0, 0);
-            panel1.Size = new Size(683, 529);
+            panel1.Size = new Size(683, 507);
             panel1.TabIndex = 0;
             // 
             // searchResultPanel
@@ -185,9 +151,23 @@
             searchResultPanel.FlowDirection = FlowDirection.TopDown;
             searchResultPanel.Location = new Point(0, 20);
             searchResultPanel.Name = "searchResultPanel";
-            searchResultPanel.Size = new Size(683, 509);
+            searchResultPanel.Size = new Size(683, 487);
             searchResultPanel.TabIndex = 0;
             searchResultPanel.WrapContents = false;
+            // 
+            // roundedPanel1
+            // 
+            roundedPanel1.BackgroundColor = Color.FromArgb(13, 18, 47);
+            roundedPanel1.BorderColor = Color.FromArgb(13, 18, 47);
+            roundedPanel1.BorderWidth = 2F;
+            roundedPanel1.Controls.Add(tableLayoutPanel1);
+            roundedPanel1.Dock = DockStyle.Fill;
+            roundedPanel1.Location = new Point(0, 0);
+            roundedPanel1.Name = "roundedPanel1";
+            roundedPanel1.Padding = new Padding(15);
+            roundedPanel1.Radius = 10;
+            roundedPanel1.Size = new Size(683, 80);
+            roundedPanel1.TabIndex = 0;
             // 
             // SearchForm
             // 
@@ -207,13 +187,12 @@
             splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
-            customPanel1.ResumeLayout(false);
-            customPanel2.ResumeLayout(false);
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            roundedPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -226,7 +205,6 @@
         private Button searchBtn;
         private TableLayoutPanel tableLayoutPanel2;
         private TextBox searchBox;
-        private CustomPanel.CustomPanel customPanel1;
-        private CustomPanel.CustomPanel customPanel2;
+        private CustomPanel.RoundedPanel roundedPanel1;
     }
 }
