e55a2d57f744b827f656625577e05c3ba0c4dd9e
Khalid <22-46601-1@student.aiub.edu>
2023-12-28 06:28:27 +0600

    Updated Mainform

--- MusicPlayer/MainForm.cs
+++ MusicPlayer/MainForm.cs
@@ -81,6 +81,7 @@ namespace MusicPlayer
             else
             {
                 musicInfoForm.UpdateMusicInfo(track, isLocal);
+                ActivateForm(musicInfoForm, null, splitContainer3.Panel2);
                 musicInfoForm.SetEmail(userEmail);
             }
         }
